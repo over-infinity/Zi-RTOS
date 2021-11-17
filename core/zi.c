a54169e1635d9a94dcd967a80c6bf53894cdb220
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-17 19:58:48 +0330

    Update zi.c
    This is how Zi-RTOS was born  :-)

--- core/zi.c
+++ core/zi.c
@@ -8,7 +8,19 @@
 
 #include "core/zi.h"
 
-void Zi_Init(void){  
+DoubleLong_t Zi_TickCount;
+static Byte_t Zi_ActiveScheduler = 0;
+
+TCB_t Zi_TaskList[ZI_MAX_TASKS ];
+TCB_t Zi_TaskSuspended[ZI_MAX_TASKS ];
+
+void Zi_Init(void){ 
+  
+  for(int i=0; i < ZI_MAX_TASKS; i++){
+    Zi_Resume_Task(i);
+  }
+  
+  Zi_TickCount=0;
   
 }  
   
@@ -36,8 +48,11 @@ void Zi_Resume_Task(Byte_t taskId){
   
 }
    
-void Zi_Delay(Long_t tick){
-  
+void Zi_Delay(Long_t tick){  
+  Long_t endTick=Zi_TickCount+tick;
+  while(Zi_TickCount < endTick){
+    ;
+  }  
 }
 
 
@@ -49,7 +64,6 @@ void Zi_Stop(){
   
 }
   
-DoubleLong_t Zi_TickCount(){
-  
-  
+DoubleLong_t Zi_GetTickCount(){  
+  return Zi_TickCount;   
 }
