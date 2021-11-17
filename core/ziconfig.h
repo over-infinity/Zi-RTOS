4b6aba9d4859220165fbbf499595e48adb8b7830
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-17 19:28:54 +0330

    Update ziconfig.h

--- core/ziconfig.h
+++ core/ziconfig.h
@@ -1,8 +1,9 @@
-/* ziconfig.h
+/* 
+* this file is a part of Zi-RTOS project, https://github.com/over-infinity/Zi-RTOS
+* Copyright (c) 2021, Over-Infinity 
 *
-*
-*
-*
+* ziconfig.h
 */
+  
 
 #define ZI_MAX_TASKS (14)
