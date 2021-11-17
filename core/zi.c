126e9c55dfae8a96c7d9f6f424d958980140b521
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-17 19:24:53 +0330

    Update zi.c

--- core/zi.c
+++ core/zi.c
@@ -5,3 +5,51 @@
 * Copyright (c) 2021, Over-Infinity 
 *
 */
+
+#include "core/zi.h"
+
+void Zi_Init(void){  
+  
+}  
+  
+void Zi_Dispath_Task(void){  
+  
+}
+
+
+Byte_t Zi_Add_Task(TaskHandler_t handler, const Word_t delay, Word_t period, Byte_t priority){ 
+  
+  
+}
+
+Byte_t Zi_Remove_Task(Byte_t){
+  
+}
+    
+void Zi_Suspend_Task(Byte_t taskId){
+  
+  
+}
+
+void Zi_Resume_Task(Byte_t taskId){
+  
+  
+}
+   
+void Zi_Delay(Long_t tick){
+  
+}
+
+
+void Zi_Start(){
+  
+}
+
+void Zi_Stop(){
+  
+}
+  
+DoubleLong_t Zi_TickCount(){
+  
+  
+}
