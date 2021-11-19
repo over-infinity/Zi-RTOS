0d54b50cfd41a543c46204c8dbee4ac0655945f5
over-infinity <over.infinity@yahoo.com>
2021-11-19 19:06:32 +0330

    complete unix example

--- examples/unix/main.cpp
+++ examples/unix/main.cpp
@@ -17,12 +17,25 @@ void Task_2(void){
 	std::cout << "Execute Task 2" << std::endl;	
 }
 
-
 int main(int argc, char** argv){
+	
+	Zi_Init();
 
-	int task1Id = Zi_Add_Task(Task_1,100000,100000,2);
-	int task2Id = Zi_Add_Task(Task_2,200000,200000,2);
+	int task1Id = Zi_Add_Task(Task_1,10000000,10000000,2);
+	int task2Id = Zi_Add_Task(Task_2,5000000,5000000,1);
+	/*executing frequency of task 2 is twice of task 1. 
+	for exit from program press ctrl+z in linux*/
 
+	Zi_Start();
+
+	while(true){
+
+		Zi_Scheduler();
+		Zi_Dispath_Task();
+
+	}
+
+	Zi_Stop();
 
 	return 0;
 }
