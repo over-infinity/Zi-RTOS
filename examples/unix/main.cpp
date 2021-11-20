d2b524d03db90b5d25906db0a1aa14494cedaba8
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-20 15:41:14 +0330

    Update main.cpp

--- examples/unix/main.cpp
+++ examples/unix/main.cpp
@@ -1,10 +1,12 @@
 
 /* 
-* *
+* 
 * this file is a part of Zi-RTOS project, https://github.com/over-infinity/Zi-RTOS
 * Copyright (c) 2021, Over-Infinity 
 *
 */
+
+/* this example shows how useing Zi-RTOS APIs for creating multi task program. */
 
 #include <iostream>
 #include "core/zi.h"
@@ -19,17 +21,20 @@ void Task_2(void){
 
 int main(int argc, char** argv){
 	
+	// Initial Zi-RTOS internal data structures
 	Zi_Init();
-
+	
+	// Add application task to the task list
 	int task1Id = Zi_Add_Task(Task_1,10000000,10000000,2);
 	int task2Id = Zi_Add_Task(Task_2,5000000,5000000,1);
 	/*executing frequency of task 2 is twice of task 1. 
 	for exit from program press ctrl+z in linux*/
 
+	// before anything else we should active Zi-RTOS core by calling Zi-Strt() API
 	Zi_Start();
 
 	while(true){
-
+		
 		Zi_Scheduler();
 		Zi_Dispath_Task();
 
