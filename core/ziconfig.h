0b80a43f1363cc36d6ef9c9e298013193b20e053
Over-Infinity <67686187+over-infinity@users.noreply.github.com>
2021-11-21 11:21:44 +0330

    Update ziconfig.h

--- core/ziconfig.h
+++ core/ziconfig.h
@@ -1,9 +1,29 @@
-/* 
-* this file is a part of Zi-RTOS project, https://github.com/over-infinity/Zi-RTOS
-* Copyright (c) 2021, Over-Infinity 
-*
-* ziconfig.h
-*/
-  
+/*
+ * this file is a part of Zi-RTOS project, https://github.com/over-infinity/Zi-RTOS
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021, Over-Infinity
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* zicinfig.h */
 
 #define ZI_MAX_TASKS (14)
